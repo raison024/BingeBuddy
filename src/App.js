7ffe60911541563fbe209acc98b5160e96a70d84
Raison Sabu <raison024@gmail.com>
2023-10-03 12:59:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Movie from "./pages/Movie";
 import Anime from "./pages/Anime";
 import Show from "./pages/Show";
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from './Theme'
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
     <Router>
       <Routes>
         <Route path="/" element={<Movie />} />
-        <Route path="/anime" element={<Anime/>} />
-        <Route path="/shows" element={<Show/>} />
+        <Route path="/anime" element={<Anime />} />
+        <Route path="/shows" element={<Show />} />
       </Routes>
     </Router>
-    </ChakraProvider>
   );
 }
 
